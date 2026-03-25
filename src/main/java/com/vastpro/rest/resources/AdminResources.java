88a4c4045d1e8f8bde93d7791438d24119b5ae89
Shushmetaa <shushmetaayethiraj0404@gmail.com>
2026-03-25 11:16:43 +0530

    Added admin resouces by shushmetaa

---
+++ src/main/java/com/vastpro/rest/resources/AdminResources.java
@@ -0,0 +1,17 @@
+package com.vastpro.rest.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/admin")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class AdminResources {
+
+	public AdminResources() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
